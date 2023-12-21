3ef3259674964891b4523d56a6a20ae0f7c0b7c4
capossele <angelocapossele@gmail.com>
2023-12-21 16:56:21 +0000

    checkpoint

--- risc0/zkvm/src/host/api/tests.rs
+++ risc0/zkvm/src/host/api/tests.rs
@@ -16,6 +16,9 @@ use std::{
     net::{SocketAddr, TcpListener},
     path::PathBuf,
     thread,
+    // io::Write,
+    // path::Path,
+    process::Command,
 };
 
 use anyhow::Result;
@@ -27,6 +30,7 @@ use test_log::test;
 
 use super::{Asset, AssetRequest, ConnectionWrapper, Connector, TcpConnection};
 use crate::{
+    Groth16Receipt, Groth16Seal,
     recursion::SuccinctReceipt, ApiClient, ApiServer, ExecutorEnv, InnerReceipt, ProverOpts,
     Receipt, SegmentReceipt, SessionInfo, VerifierContext,
 };
@@ -219,6 +223,176 @@ fn lift_join_identity() {
 
     let rollup_receipt = Receipt::new(InnerReceipt::Succinct(rollup), session.journal.bytes.into());
     rollup_receipt.verify(MULTI_TEST_ID).unwrap();
+}
+
+#[test]
+fn stark2snark() {
+    // const CEREMONY_DIR: &str = "ceremony";
+    const SEAL_FILE: &str = "seal.bin";
+    // const WITNESS_FILE: &str = "output.wtns";
+    // const PROOF_FILE: &str = "proof.json";
+    // const PUBLIC_FILE: &str = "public.json";
+
+    let cycles = 0u32;
+    let env = ExecutorEnv::builder()
+        .write(&MultiTestSpec::BusyLoop { cycles })
+        .unwrap()
+        .build()
+        .unwrap();
+    let binary = Asset::Inline(MULTI_TEST_ELF.into());
+
+    let mut client = TestClient::new();
+
+    let session = client.execute(env, binary);
+    assert_eq!(session.segments.len(), client.segments.len());
+
+    let opts = ProverOpts::default();
+
+    let receipt = client.prove_segment(opts.clone(), client.segments[0].clone());
+    let mut rollup = client.lift(opts.clone(), receipt.try_into().unwrap());
+
+    for segment in &client.segments[1..] {
+        let receipt = client.prove_segment(opts.clone(), segment.clone());
+        let rec_receipt = client.lift(opts.clone(), receipt.try_into().unwrap());
+
+        rollup = client.join(
+            opts.clone(),
+            rollup.try_into().unwrap(),
+            rec_receipt.try_into().unwrap(),
+        );
+        rollup
+            .verify_integrity_with_context(&VerifierContext::default())
+            .unwrap();
+    }
+    let receipt_ident = client.identity_p254(opts, rollup.clone().try_into().unwrap());
+
+    let work_dir = tempdir().expect("Failed to create tmpdir");
+    let seal_path = work_dir.path().join(SEAL_FILE);
+    std::fs::write(&seal_path, &receipt_ident.get_seal_bytes())
+        .expect("Failed to write seal-to-json stdout to disk");
+
+    let journal = session.journal.bytes;
+
+    let rollup_receipt = Receipt::new(InnerReceipt::Succinct(rollup), journal.clone());
+    rollup_receipt.verify(MULTI_TEST_ID).unwrap();
+
+    // let ceremony_path = Path::new("../..").join(CEREMONY_DIR);
+    // if !ceremony_path.exists() {
+    //     panic!("Missing ceremony path");
+    // }
+
+    // let seal_to_json_path = ceremony_path.join("seal-to-json");
+
+    // println!("Running seal-to-json");
+    // let mut child = Command::new(seal_to_json_path)
+    //     .stdin(Stdio::piped())
+    //     .stdout(Stdio::piped())
+    //     .spawn()
+    //     .unwrap();
+
+    // if let Some(mut stdin) = child.stdin.take() {
+    //     stdin.write_all(&receipt_ident.get_seal_bytes()).unwrap();
+    // }
+
+    // let output = child.wait_with_output().unwrap();
+    // if !output.status.success() {
+    //     panic!("Failed to run seal-to-json");
+    // }
+
+    // println!("Running verify");
+    // let verify_bin = ceremony_path.join("verify_cpp").join("verify");
+    // let witness_path = work_dir.path().join(WITNESS_FILE);
+    // let child = Command::new(verify_bin)
+    //     .arg(&seal_path)
+    //     .arg(&witness_path)
+    //     .spawn()
+    //     .unwrap();
+
+    // let res = child.wait_with_output().unwrap();
+    // if !res.status.success() {
+    //     panic!("Failed to run verify");
+    // }
+
+    // println!("Running rapidsnark");
+    // let zkey_file = ceremony_path.join("verify_final.zkey");
+    // let proof_file = work_dir.path().join(PROOF_FILE);
+    // let public_file = work_dir.path().join(PUBLIC_FILE);
+
+    // let child = Command::new("rapidsnark")
+    //     .arg(zkey_file)
+    //     .arg(witness_path)
+    //     .arg(&proof_file)
+    //     .arg(&public_file)
+    //     .spawn()
+    //     .unwrap();
+    // let res = child.wait_with_output().unwrap();
+    // if !res.status.success() {
+    //     panic!("Failed to run rapidsnark");
+    // }
+
+    // let output = Command::new("snarkjs")
+    //     .arg("zkey")
+    //     .arg("export")
+    //     .arg("soliditycalldata")
+    //     .arg(&public_file)
+    //     .arg(&proof_file)
+    //     .output()
+    //     .unwrap();
+    // if !output.status.success() {
+    //     panic!("Failed to run snarkjs");
+    // }
+
+    let output = Command::new("docker")
+        .arg("run")
+        .arg("--rm")
+        .arg("-v")
+        .arg(&format!("{:}:/app/seal.bin:ro", seal_path.to_string_lossy()))
+        .arg("prover")
+        .output()
+        .unwrap();
+
+    let snark_str = String::from_utf8(output.stdout).unwrap();
+    let snark_str = format!("[{snark_str}]"); // make the output valid json
+    println!("{snark_str}");
+
+    let raw_proof: (Vec<String>, Vec<Vec<String>>, Vec<String>, Vec<String>) =
+        serde_json::from_str(&snark_str).unwrap();
+    let a: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
+        .0
+        .into_iter()
+        .map(|elm| hex::decode(&elm[2..]))
+        .collect();
+    let a = a.expect("Failed to decode snark 'a' values");
+
+    let b: Result<Vec<Vec<Vec<u8>>>, hex::FromHexError> = raw_proof
+        .1
+        .into_iter()
+        .map(|inner| {
+            inner
+                .into_iter()
+                .map(|elm| hex::decode(&elm[2..]))
+                .collect::<Result<Vec<Vec<u8>>, hex::FromHexError>>()
+        })
+        .collect();
+    let b = b.expect("Failed to decode snark 'b' values");
+
+    let c: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
+        .2
+        .into_iter()
+        .map(|elm| hex::decode(&elm[2..]))
+        .collect();
+    let c = c.expect("Failed to decode snark 'c' values");
+
+    let groth16_seal = Groth16Seal{a, b, c};    
+    let receipt = Receipt::new(
+            InnerReceipt::Groth16(Groth16Receipt {
+                seal: groth16_seal.to_vec(),
+                claim: rollup_receipt.get_claim().unwrap(),
+            }),
+            journal,
+        );
+    
+    receipt.verify(MULTI_TEST_ID).unwrap();
 }
 
 #[test]
