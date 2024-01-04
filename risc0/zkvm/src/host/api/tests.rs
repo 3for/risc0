e3874534a5345f216860980782fd39f3a0fb26ce
capossele <angelocapossele@gmail.com>
2024-01-04 15:53:35 +0000

    fix imports

--- risc0/zkvm/src/host/api/tests.rs
+++ risc0/zkvm/src/host/api/tests.rs
@@ -240,109 +240,6 @@ fn lift_join_identity() {
     rollup_receipt.verify(MULTI_TEST_ID).unwrap();
 }
 
-#[cfg(feature = "docker")]
-#[test]
-fn stark2snark() {
-    const SEAL_FILE: &str = "seal.bin";
-
-    let cycles = 0u32;
-    let env = ExecutorEnv::builder()
-        .write(&MultiTestSpec::BusyLoop { cycles })
-        .unwrap()
-        .build()
-        .unwrap();
-    let binary = Asset::Inline(MULTI_TEST_ELF.into());
-
-    let mut client = TestClient::new();
-
-    let session = client.execute(env, binary);
-    assert_eq!(session.segments.len(), client.segments.len());
-
-    let opts = ProverOpts::default();
-
-    let receipt = client.prove_segment(opts.clone(), client.segments[0].clone());
-    let mut rollup = client.lift(opts.clone(), receipt.try_into().unwrap());
-
-    for segment in &client.segments[1..] {
-        let receipt = client.prove_segment(opts.clone(), segment.clone());
-        let rec_receipt = client.lift(opts.clone(), receipt.try_into().unwrap());
-
-        rollup = client.join(
-            opts.clone(),
-            rollup.try_into().unwrap(),
-            rec_receipt.try_into().unwrap(),
-        );
-        rollup
-            .verify_integrity_with_context(&VerifierContext::default())
-            .unwrap();
-    }
-    let receipt_ident = client.identity_p254(opts, rollup.clone().try_into().unwrap());
-
-    let work_dir = tempdir().expect("Failed to create tmpdir");
-    let seal_path = work_dir.path().join(SEAL_FILE);
-    std::fs::write(&seal_path, &receipt_ident.get_seal_bytes())
-        .expect("Failed to write seal-to-json stdout to disk");
-
-    let journal = session.journal.bytes;
-
-    let rollup_receipt = Receipt::new(InnerReceipt::Succinct(rollup), journal.clone());
-    rollup_receipt.verify(MULTI_TEST_ID).unwrap();
-
-    let output = Command::new("docker")
-        .arg("run")
-        .arg("--rm")
-        .arg("-v")
-        .arg(&format!(
-            "{:}:/app/seal.bin:ro",
-            seal_path.to_string_lossy()
-        ))
-        .arg("angelocapossele/risc0-groth16-prover:v0.0.1")
-        .output()
-        .unwrap();
-
-    let snark_str = String::from_utf8(output.stdout).unwrap();
-    let snark_str = format!("[{snark_str}]"); // make the output valid json
-
-    let raw_proof: (Vec<String>, Vec<Vec<String>>, Vec<String>, Vec<String>) =
-        serde_json::from_str(&snark_str).unwrap();
-    let a: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
-        .0
-        .into_iter()
-        .map(|elm| hex::decode(&elm[2..]))
-        .collect();
-    let a = a.expect("Failed to decode snark 'a' values");
-
-    let b: Result<Vec<Vec<Vec<u8>>>, hex::FromHexError> = raw_proof
-        .1
-        .into_iter()
-        .map(|inner| {
-            inner
-                .into_iter()
-                .map(|elm| hex::decode(&elm[2..]))
-                .collect::<Result<Vec<Vec<u8>>, hex::FromHexError>>()
-        })
-        .collect();
-    let b = b.expect("Failed to decode snark 'b' values");
-
-    let c: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
-        .2
-        .into_iter()
-        .map(|elm| hex::decode(&elm[2..]))
-        .collect();
-    let c = c.expect("Failed to decode snark 'c' values");
-
-    let groth16_seal = Groth16Seal { a, b, c };
-    let receipt = Receipt::new(
-        InnerReceipt::Groth16(Groth16Receipt {
-            seal: groth16_seal.to_vec(),
-            claim: rollup_receipt.get_claim().unwrap(),
-        }),
-        journal,
-    );
-
-    receipt.verify(MULTI_TEST_ID).unwrap();
-}
-
 #[test]
 fn lift_resolve() {
     let mut client = TestClient::new();
@@ -423,4 +320,112 @@ fn guest_error_forwarding() {
         .unwrap();
     let binary = Asset::Inline(MULTI_TEST_ELF.into());
     TestClient::new().execute(env, binary);
+}
+
+#[cfg(feature = "docker")]
+mod tests {
+    use crate::{Groth16Receipt, Groth16Seal};
+    use std::process::Command;
+
+    #[test]
+    fn stark2snark() {
+        const SEAL_FILE: &str = "seal.bin";
+
+        let cycles = 0u32;
+        let env = ExecutorEnv::builder()
+            .write(&MultiTestSpec::BusyLoop { cycles })
+            .unwrap()
+            .build()
+            .unwrap();
+        let binary = Asset::Inline(MULTI_TEST_ELF.into());
+
+        let mut client = TestClient::new();
+
+        let session = client.execute(env, binary);
+        assert_eq!(session.segments.len(), client.segments.len());
+
+        let opts = ProverOpts::default();
+
+        let receipt = client.prove_segment(opts.clone(), client.segments[0].clone());
+        let mut rollup = client.lift(opts.clone(), receipt.try_into().unwrap());
+
+        for segment in &client.segments[1..] {
+            let receipt = client.prove_segment(opts.clone(), segment.clone());
+            let rec_receipt = client.lift(opts.clone(), receipt.try_into().unwrap());
+
+            rollup = client.join(
+                opts.clone(),
+                rollup.try_into().unwrap(),
+                rec_receipt.try_into().unwrap(),
+            );
+            rollup
+                .verify_integrity_with_context(&VerifierContext::default())
+                .unwrap();
+        }
+        let receipt_ident = client.identity_p254(opts, rollup.clone().try_into().unwrap());
+
+        let work_dir = tempdir().expect("Failed to create tmpdir");
+        let seal_path = work_dir.path().join(SEAL_FILE);
+        std::fs::write(&seal_path, &receipt_ident.get_seal_bytes())
+            .expect("Failed to write seal-to-json stdout to disk");
+
+        let journal = session.journal.bytes;
+
+        let rollup_receipt = Receipt::new(InnerReceipt::Succinct(rollup), journal.clone());
+        rollup_receipt.verify(MULTI_TEST_ID).unwrap();
+
+        let output = Command::new("docker")
+            .arg("run")
+            .arg("--rm")
+            .arg("-v")
+            .arg(&format!(
+                "{:}:/app/seal.bin:ro",
+                seal_path.to_string_lossy()
+            ))
+            .arg("angelocapossele/risc0-groth16-prover:v0.0.1")
+            .output()
+            .unwrap();
+
+        let snark_str = String::from_utf8(output.stdout).unwrap();
+        let snark_str = format!("[{snark_str}]"); // make the output valid json
+
+        let raw_proof: (Vec<String>, Vec<Vec<String>>, Vec<String>, Vec<String>) =
+            serde_json::from_str(&snark_str).unwrap();
+        let a: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
+            .0
+            .into_iter()
+            .map(|elm| hex::decode(&elm[2..]))
+            .collect();
+        let a = a.expect("Failed to decode snark 'a' values");
+
+        let b: Result<Vec<Vec<Vec<u8>>>, hex::FromHexError> = raw_proof
+            .1
+            .into_iter()
+            .map(|inner| {
+                inner
+                    .into_iter()
+                    .map(|elm| hex::decode(&elm[2..]))
+                    .collect::<Result<Vec<Vec<u8>>, hex::FromHexError>>()
+            })
+            .collect();
+        let b = b.expect("Failed to decode snark 'b' values");
+
+        let c: Result<Vec<Vec<u8>>, hex::FromHexError> = raw_proof
+            .2
+            .into_iter()
+            .map(|elm| hex::decode(&elm[2..]))
+            .collect();
+        let c = c.expect("Failed to decode snark 'c' values");
+
+        let groth16_seal = Groth16Seal { a, b, c };
+        let receipt = Receipt::new(
+            InnerReceipt::Groth16(Groth16Receipt {
+                seal: groth16_seal.to_vec(),
+                claim: rollup_receipt.get_claim().unwrap(),
+            }),
+            journal,
+        );
+
+        receipt.verify(MULTI_TEST_ID).unwrap();
+    }
 }
